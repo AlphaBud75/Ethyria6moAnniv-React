9dda0234d7e7b67e73281b28a8d54fcd54a9a87e
AlphaBud75 <100396124+AlphaBud75@users.noreply.github.com>
2022-04-04 00:58:12 -0500

    deploy initiate test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import eiemotes from './ei/img_01.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={eiemotes} alt="eiemotes"></img>
+      <h1>deploy success!!!!!!!</h1>
     </div>
   );
 }

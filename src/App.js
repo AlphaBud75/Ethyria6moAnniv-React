e6dc2d2ab97cdfe9fe28139bd4a6fefaaf47a23e
AlphaBud75 <100396124+AlphaBud75@users.noreply.github.com>
2022-04-04 01:07:04 -0500

    deploy initiate test 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import eiemotes from './ei/img_01.jpg'
+import eiemotes01 from './ei/img_01.jpg'
+import eiemotes02 from './ei/img_02.jpg'
+import eiemotes03 from './ei/img_03.jpg'
+import eiemotes04 from './ei/img_04.jpg'
+import eiemotes05 from './ei/img_05.jpg'
 
 function App() {
   return (
     <div className="App">
-      <img src={eiemotes} alt="eiemotes"></img>
+      <img src={eiemotes01} alt="eiemotes"></img>
+      <img src={eiemotes02} alt="eiemotes"></img>
+      <img src={eiemotes03} alt="eiemotes"></img>
+      <img src={eiemotes04} alt="eiemotes"></img>
+      <img src={eiemotes05} alt="eiemotes"></img>
       <h1>deploy success!!!!!!!</h1>
     </div>
   );
